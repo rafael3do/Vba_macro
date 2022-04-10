15bdd2bca86da6e30c9bab744735395e85ddc8d2
Rafael Figueiredo <rafael3do@hotmail.com>
2022-04-10 10:34:43 -0300

    Adequation variables

--- Vba_macro/Program.cs
+++ Vba_macro/Program.cs
@@ -10,27 +10,27 @@ namespace Vba_macro
         static void Main(string[] args)
         {//abaixo inicialização das variaveis.
             //below variable initialization.
-            string Pasta = @"C:\Temp";
-            int Linhas = 0;
-            int Val_teste = 1;
-            int Sub_ciclo = 0;
-            string Num_linhas = "";
-            int Valida_escrita = 0;
+            string pasta = @"C:\Temp";
+            int linhas = 0;
+            int val_teste = 1;
+            int sub_ciclo = 0;
+            string num_linhas = "";
+            int valida_escrita = 0;
             int escrevi = 1;
-            if (!File.Exists(Pasta))//valida se tem a pasta temp caso contrario ele cria.
+            if (!File.Exists(pasta))//valida se tem a pasta temp caso contrario ele cria.
                                     // validate if it has temp folder otherwise it creates.
             {
-                System.IO.Directory.CreateDirectory(Pasta);
+                System.IO.Directory.CreateDirectory(pasta);
             }
             FileStream Arquivo = new FileStream(@"C:\Temp\Codigo_vba_macro.txt", FileMode.Create);
             StreamWriter Insere = new StreamWriter(Arquivo);//IO de escrita.
                                                             // IO write.
-            while (Linhas == 0)//valida as linhas digitadas.
+            while (linhas == 0)//valida as linhas digitadas.
                                // validate the typed lines.
             {
                 Console.WriteLine("Digite a quantidade de linhas na planilha.");
-                Num_linhas = Console.ReadLine();
-                bool Valida_numero = int.TryParse(Num_linhas, out Linhas);
+                num_linhas = Console.ReadLine();
+                bool Valida_numero = int.TryParse(num_linhas, out linhas);
                 if (Valida_numero == false)
                 {
                     Console.WriteLine("Numero invalido.");
@@ -41,31 +41,31 @@ namespace Vba_macro
                     Insere.WriteLine(" ");
                 }
             }
-            for (int i = 0; i <= Linhas; i += 200)//Crias as sub consultas do vba.
+            for (int i = 0; i <= linhas; i += 200)//Crias as sub consultas do vba.
                                                   // Create vba sub queries.
             {
-                Insere.WriteLine("Call Ciclo" + Val_teste);
+                Insere.WriteLine("Call Ciclo" + val_teste);
                 Insere.WriteLine(" ");
-                Val_teste += 1;
+                val_teste += 1;
             }
             Insere.WriteLine("End Sub");
             Insere.WriteLine(" ");
-            for (Sub_ciclo = 1; Sub_ciclo < Linhas; Sub_ciclo++)//Digita a linha dentro das sub consultas do vba.
+            for (sub_ciclo = 1; sub_ciclo < linhas; sub_ciclo++)//Digita a linha dentro das sub consultas do vba.
                                                                 // Type the line inside vba sub queries.
             {
                 Contador();
-                Insere.WriteLine("Range(\"E" + (Sub_ciclo + 1) + ":H" + (Sub_ciclo + 1) + "\").Select");
+                Insere.WriteLine("Range(\"E" + (sub_ciclo + 1) + ":H" + (sub_ciclo + 1) + "\").Select");
                 Insere.WriteLine("Application.CutCopyMode = False");
                 Insere.WriteLine("Selection.Cut");
-                Insere.WriteLine("Rows(\"" + (1 + (2 * Sub_ciclo)) + ":" + (1 + (2 * Sub_ciclo) + "\").Select"));
+                Insere.WriteLine("Rows(\"" + (1 + (2 * sub_ciclo)) + ":" + (1 + (2 * sub_ciclo) + "\").Select"));
                 Insere.WriteLine("Selection.Insert Shift:=xlDown");
                 Insere.WriteLine(" ");
-                Valida_escrita++;
+                valida_escrita++;
             }
             void Contador()//Funçao dentro da classe que inicia o espaço para digitar as linhas organizando nas funções.
                            // Function within class that starts the space to type lines by arranging in functions.
             {
-                if (Valida_escrita == 0)
+                if (valida_escrita == 0)
                 {
                     for (int i = 0; i < 1; i++)
                     {
@@ -76,9 +76,9 @@ namespace Vba_macro
                         Insere.WriteLine(" ");
                     }
                     escrevi += 1;
-                    Valida_escrita = 1;
+                    valida_escrita = 1;
                 }
-                if (Valida_escrita == 200)
+                if (valida_escrita == 200)
                 {
                     Insere.WriteLine("Next i");
                     Insere.WriteLine("End Sub");
@@ -88,7 +88,7 @@ namespace Vba_macro
                     Insere.WriteLine("For i = 1 To 1");
                     Insere.WriteLine(" ");
                     escrevi += 1;
-                    Valida_escrita = 1;
+                    valida_escrita = 1;
                 }
             }
             Insere.WriteLine("Next i");
